32bafe7ed3bae2a81c0673a58d250d4af057d0e7
v5ctor <victorbitt00@gmail.com>
2020-04-07 15:05:53 -0300

    Criado nome idade em pessoa

--- object/helplom.py
+++ object/helplom.py
@@ -1,2 +1,17 @@
 class Pessoa:
-    pass
+    def __init__(self,nome=None,idade=None):
+        self.nome=nome
+        self.idade=idade
+
+    def digaoi(self):
+        return f'Oi,{id(self)}'
+if __name__ == '__main__':
+    p = Pessoa('Reinaldo',34)
+    print(p.nome,p.idade)
+    print(Pessoa.digaoi(p))
+    print(p.digaoi())
+    p.nome='Víctor'
+    p.idade=19
+    print(p.nome)
+    print(p.idade)
+    print(id(p))
